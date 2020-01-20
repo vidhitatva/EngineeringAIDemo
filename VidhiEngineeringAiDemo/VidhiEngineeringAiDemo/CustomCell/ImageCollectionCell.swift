1810ab762cc74a4f548157a0a8e6d102bd1f2e23
Vidhi Patel <vidhi.patel@tatvasoft.com>
2020-01-20 15:22:29 +0530

    Design User View

---
+++ VidhiEngineeringAiDemo/VidhiEngineeringAiDemo/CustomCell/ImageCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionCell.swift
+//  VidhiEngineeringAiDemo
+//
+//  Created by MAC105 on 20/01/20.
+//  Copyright © 2020 MAC105. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ VidhiEngineeringAiDemo/VidhiEngineeringAiDemo/CustomCell/UserTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableCell.swift
+//  VidhiEngineeringAiDemo
+//
+//  Created by MAC105 on 20/01/20.
+//  Copyright © 2020 MAC105. All rights reserved.
+//
+
+import UIKit
+
+class UserTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
